bbd7eaade703188ef489bc98cec6d6baafd982b3
autopilot-paas <svarakhedi@pivotal.io>
2014-10-23 19:04:42 -0700

    Move install pre-reqs under autopilot

---
+++ setup/install_libs.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+# install autopilot pre-reqs
+yum install -y python-devel
+yum install -y libevent-devel
--- test/vagrant/provision.sh
+++ test/vagrant/provision.sh
@@ -1,11 +1,9 @@
-#! /bin/sh
+#!/bin/sh
 
 yum install -y epel-release
 yum install -y xz
 yum install -y python-pip
 yum install -y git
-yum install -y python-devel
-yum install -y libevent-devel
 
 # python 2.7
 wget http://python.org/ftp/python/2.7.5/Python-2.7.5.tar.xz -q
@@ -13,7 +11,6 @@ tar xf Python-2.7.5.tar.xz
 pushd Python-2.7.5
 ./configure --prefix=/usr/local --enable-unicode=ucs4 --enable-shared LDFLAGS="-Wl,-rpath /usr/local/lib"
 make && make altinstall
-cd ..
 popd
 
 #virtualenv
@@ -21,18 +18,14 @@ pushd /home/vagrant/
 pip install virtualenv
 virtualenv vtest -p /usr/local/bin/python2.7
 source vtest/bin/activate
-
-# install autopilot pre-reqs
-pip install -r requirements.txt
-
 chown -R vagrant:vagrant vtest
 
-popd
-
+# get autopilot
 git clone https://github.com/autopilot-paas/autopilot.git
+chown -R vagrant:vagrant autopilot
 
-cd autopilot
+# ap pre-reqs
+/bin/sh ./autopilot/setup/install_libs.sh
+pip install -r ./autopilot/setup/requirements.txt
 
-#source autopilot_env.sh
-#
-#source /vagrant/.awsaccess
+popd
